4da1b8e2fa94cb724cde8bca93405476e83139b2
Denny Bucklin <dennis.bucklin@gmail.com>
2024-07-13 22:05:18 -0500

    added arrows

--- game/player.go
+++ game/player.go
@@ -35,37 +35,48 @@ func NewPlayer() *Player {
 
 func (p *Player) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(
+		-(float64(p.sprite.Bounds().Dx())/2),
+		-(float64(p.sprite.Bounds().Dy())/2),
+	)
 	op.GeoM.Rotate(p.position.Dir)
-	op.GeoM.Translate(p.position.X-20, p.position.Y-20)
+	op.GeoM.Translate(p.position.X, p.position.Y)
 	screen.DrawImage(p.sprite, op)
 }
 
 func (p *Player) Move() {
 	speed := float64(200 / ebiten.TPS())
-	var Y float64 = 0
-	var X float64 = 0
+	var y float64 = 0
+	var x float64 = 0
+	var dir float64 = 0
 	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		Y += speed
+		y += speed
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		Y -= speed
+		y -= speed
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
-		X -= speed
+		x -= speed
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyD) {
-		X += speed
+		x += speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		dir -= 0.1
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		dir += 0.1
 	}
 
 	// Check for diagonal movement
-	if X != 0 && Y != 0 {
-		factor := speed / math.Sqrt(X*X+Y*Y)
-		X *= factor
-		Y *= factor
+	if x != 0 && y != 0 {
+		factor := speed / math.Sqrt(x*x+y*y)
+		x *= factor
+		y *= factor
 	}
 
-	p.position.X += X
-	p.position.Y += Y
+	p.position.X += x
+	p.position.Y += y
 
 	if p.position.X < 0 {
 		p.position.X = 0
@@ -78,5 +89,13 @@ func (p *Player) Move() {
 	}
 	if p.position.Y > ScreenHeight {
 		p.position.Y = ScreenHeight
+	}
+
+	p.position.Dir += dir
+	if p.position.Dir > 2*math.Pi {
+		p.position.Dir -= 2*math.Pi
+	}
+	if p.position.Dir < 2*math.Pi {
+		p.position.Dir += 2*math.Pi
 	}
 }
